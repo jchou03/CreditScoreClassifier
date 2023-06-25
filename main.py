d97a87928b663f624cdf5d5ce429206cb150656e
jchou21 <jared.chou7@gmail.com>
2023-06-25 05:01:51 -0400

    using RandomForestClassifier from sklearn to classify data

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.io as pio
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 pio.templates.default = "plotly_white"
 
 # machine learning basic projects
@@ -54,3 +56,22 @@ fig = px.box(data,
 
 fig.update_traces(quartilemethod="exclusive")
 fig.show()
+
+# map credit mix column to a number for ML model
+data["Credit_Mix"] = data["Credit_Mix"].map({"Standard": 1,
+                                             "Good": 2,
+                                             "Bad": 0})
+
+x = np.array(data[["Annual_Income", "Monthly_Inhand_Salary", 
+                   "Num_Bank_Accounts", "Num_Credit_Card", 
+                   "Interest_Rate", "Num_of_Loan", 
+                   "Delay_from_due_date", "Num_of_Delayed_Payment", 
+                   "Credit_Mix", "Outstanding_Debt", 
+                   "Credit_History_Age", "Monthly_Balance"]])
+
+y = np.array(data[["Credit_Score"]])
+
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.33, random_state=42)
+
+model = RandomForestClassifier()
+model.fit(xtrain, ytrain)
