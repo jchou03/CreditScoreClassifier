55be92ca8a4b32bf6b2d17c71bf25dcf2ce7dd53
jchou21 <jared.chou7@gmail.com>
2023-06-25 04:49:18 -0400

    additional data visualizations to better understand impact of different columns of data

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ pio.templates.default = "plotly_white"
 # https://thecleverprogrammer.com/2022/12/05/credit-score-classification-with-machine-learning/
 
 data = pd.read_csv("Credit Score Data/train.csv")
+color_discrete_map = {'Poor':'red',
+                      'Standard':'yellow',
+                      'Good':'green'}
 # print(data.head())
 
 # print(data.info())
@@ -16,14 +19,38 @@ data = pd.read_csv("Credit Score Data/train.csv")
 print(data["Credit_Score"].value_counts())
 
 # exploring different aspects of the occupation data
-fig = px.box(data, 
-             x="Occupation", 
-             color="Credit_Score", 
-             title="Credit Score Based on Occupation",
-             color_discrete_map={
-                 'Poor': 'red',
-                 'Standard': 'yellow',
-                 'Good': 'green'
-             })
+# fig = px.box(data, 
+#              x="Occupation", 
+#              color="Credit_Score", 
+#              title="Credit Score Based on Occupation",
+#              color_discrete_map={
+#                  'Poor': 'red',
+#                  'Standard': 'yellow',
+#                  'Good': 'green'
+#              })
 
+# fig = px.box(data,
+#              x="Credit_Score",
+#              y="Annual_Income",
+#              color="Credit_Score",
+#              title="Credit Scores Based on Annual Income",
+#              color_discrete_map={'Poor':'red',
+#                                  'Standard':'yellow',
+#                                  'Good':'green'})
+
+# fig = px.box(data,
+#              x="Credit_Score",
+#              y="Monthly_Inhand_Salary",
+#              color="Credit_Score",
+#              title="Credit Scores Based on Monthly Inhand Salary",
+#              color_discrete_map=color_discrete_map)
+
+fig = px.box(data,
+             x="Credit_Score",
+             y="Num_Bank_Accounts",
+             color="Credit_Score",
+             title="Credit Score based on number of bank accounts",
+             color_discrete_map=color_discrete_map)
+
+fig.update_traces(quartilemethod="exclusive")
 fig.show()
